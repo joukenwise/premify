bb124fdea96c8f55b1c3b98022c0ec79dcf4e14c
joukenwise <sonupaisea@gmail.com>
2025-09-05 11:32:11 +0700

    Update products.js

--- products.js
+++ products.js
@@ -4,9 +4,9 @@ const products = [
     category: "streaming", 
     img: "https://i.ibb.co.com/2YMSpYCB/images-7.png",
     variants: [
-      { variant: "Sharing 1p2u", durasi: "1 Bulan", price: 18000 },
-      { variant: "Sharing 1p1u", durasi: "1 Bulan", price: 25000 },
-      { variant: "Semi Private", durasi: "1 Bulan", price: 28000 }
+      { variant: "Sharing 1p2u", durasi: "1 Bulan", price: 16000 },
+      { variant: "Sharing 1p1u", durasi: "1 Bulan", price: 23000 },
+      { variant: "Semi Private", durasi: "1 Bulan", price: 26000 }
     ]
   },
   {
@@ -15,12 +15,12 @@ const products = [
     img: "https://i.ibb.co.com/nK2LsBG/download-8.jpg",
     variants: [
       { variant: "Member Pro", durasi: "2 Bulan", price: 3000 },
-      { variant: "Member Pro", durasi: "1 Tahun", price: 14000 },
-     { variant: "Member Edu", durasi: "LIFETIME", price: 19000 },
-     { variant: "Member Pro Admin", durasi: "1 Bulan", price: 8000 },
-     { variant: "Member Pro Admin", durasi: "2 Bulan", price: 17000 },
-     { variant: "Head", durasi: "1 Bulan", price: 13000 },
-     { variant: "Head", durasi: "2 Bulan", price: 20000 }
+      { variant: "Member Pro", durasi: "1 Tahun", price: 12000 },
+     { variant: "Member Edu", durasi: "LIFETIME", price: 12000 },
+     { variant: "Member Pro Admin", durasi: "1 Bulan", price: 6000 },
+     { variant: "Member Pro Admin", durasi: "2 Bulan", price: 15000 },
+     { variant: "Head", durasi: "1 Bulan", price: 11000 },
+     { variant: "Head", durasi: "2 Bulan", price: 18000 }
     ]
   },
   {
@@ -28,10 +28,10 @@ const products = [
     category: "other", 
     img: "https://i.ibb.co.com/WpHBbTqn/images-22.png",
     variants: [
-      { variant: "Student No Garansi", durasi: "1 Bulan", price: 15000 },
-      { variant: "Student Garansi 7d", durasi: "1 Bulan", price: 20000 },
-      { variant: "Student Garansi Full", durasi: "1 Bulan", price: 32000 },
-      { variant: "Student IndPlan No Garansi", durasi: "4 Bulan", price: 34000 }
+      { variant: "Student No Garansi", durasi: "1 Bulan", price: 13000 },
+      { variant: "Student Garansi 7d", durasi: "1 Bulan", price: 18000 },
+      { variant: "Student Garansi Full", durasi: "1 Bulan", price: 30000 },
+      { variant: "Student IndPlan No Garansi", durasi: "4 Bulan", price: 32000 }
     ]
   },
   {
@@ -39,8 +39,8 @@ const products = [
     category: "editing", 
     img: "https://i.ibb.co.com/tPbFr8Dj/c410c7c8c7bb31013500d214a7da8a8b.jpg",
     variants: [
-      { variant: "Sharing", durasi: "1 Bulan", price: 13000 },
-      { variant: "Private", durasi: "1 Bulan", price: 16000 }
+      { variant: "Sharing", durasi: "1 Bulan", price: 11000 },
+      { variant: "Private", durasi: "1 Bulan", price: 14000 }
     ]
   }
 ];
